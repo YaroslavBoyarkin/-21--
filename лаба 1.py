c9eaa9874688b7e2cc8ec8d97a8174828b5f8bee
BoyarkinYaroslav <yabokuu11@gmail.com>
2026-09-10 21:50:22 +0300

    Add files via upload

---
+++ лаба 1.py
@@ -0,0 +1,44 @@
+#перше завдання
+
+print ('Hello World')
+
+#типи данних і змінні
+
+number = 25                #ціле число
+decimal = 6.28             #дробове число
+text = "Солдат"            #рядок
+status = False             #логічне значення
+numbers = [2, 4, 6, 8, 10] #список
+
+#виведення значень іх типів
+
+print(number, type(number))
+print(decimal, type(decimal))
+print(text, type(text))
+print(status, type(status))
+print(numbers, type(numbers))
+
+#оператори
+
+#створюємо два числа
+x = 8
+y = 5
+
+#арифметика(+, -, *, /, %, **, //)
+
+print(x + y)   # додавання
+print(x - y)   # віднімання
+print(x * y)   # множення
+print(x / y)   # звичайне ділення
+print(x // y)  # цілочисельне ділення
+print(x % y)   # залишок від ділення
+print(x ** y)  # піднесення до степеня
+
+#оператори порівняння(>, <, >=, <=, ==, !=)
+
+print(x > y)   # більше
+print(x < y)   # менше
+print(x >= y)  # більше або дорівнює
+print(x <= y)  # менше або дорівнює
+print(x == y)  # дорівнює
+print(x != y)  # не дорівнює
